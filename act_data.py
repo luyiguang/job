34d3c94957932e9c442c60ed7bf1cda49282c5e2
rodger <luyiguang@yeah.net>
2020-06-01 18:58:02 +0800

    job2

--- act_data.py
+++ act_data.py
@@ -3,8 +3,14 @@
 # @Author: rodgerlu
 import os
 import time
+import datetime
+
 import pymysql
 import json
+
+event_type_dic = {'6': 'first_rech', '7': 'beginner_accu', '8': 'beginner_date', '9': 'date_rech',
+                  '10': 'accumlate_rech', '11': 'user_accu', '12': 'dragon_trial', '13': 'battle_pass',
+                  '14': 'battle_of', '15': 'alliance_war', '16': 'joy_event'}
 
 
 def get_record(file, mand):
@@ -12,7 +18,7 @@ def get_record(file, mand):
         for line in f:
             line = line.replace('\n', '')
             a = line.split('\t')
-            time = a[0].replace('[', '').replace(']', '')
+            data_time = a[0].replace('[', '').replace(']', '')
             b = a[5].split('|')
             if mand == b[0]:
                 c = b[2:]
@@ -20,13 +26,21 @@ def get_record(file, mand):
                     if ",0" in i:
                         if mand == 'add_score':
                             user = b[1]
-                            act = b[2:]
-                            yield time, user, act
+                            acts = b[2:]
+                            act = []
+                            for j in acts:
+                                if ',0' in j:
+                                    act.append(j)
+                            yield data_time, user, act
                         if mand == 'batch_add_score':
                             b[1] = json.loads(b[1])
                             user = b[1][int(b[2])]
-                            act = b[3:]
-                            yield time, user, act
+                            acts = b[3:]
+                            act = []
+                            for j in acts:
+                                if ',0' in j:
+                                    act.append(j)
+                            yield data_time, user, act
                         break
 
 
@@ -41,30 +55,61 @@ def get_files_name(dir):
                 yield file_name
 
 
-if __name__ == "__main__":
-    # Read fime name
-    file_name = r'E:\sCrpDownload\event_center_new_2020052812.log_172.31.4.144.log'
-    mand_one = 'add_score'
-    mand_two = 'batch_add_score'
-    dir = r"E:\sCrpDownload"
-    for i in get_files_name(dir):
-        record = get_record(os.path.join(dir, i), mand_two)
-        for time, user, act in record:
-            print(time, user)
-            acts =[]
-            for j in act:
-                if ',0' in j:
-                  acts.append(j)
-            print(act)
-            print(acts)
-    # config = {'host':"localhost",'port':3306,'password':'root','db':'rodger',"user":'root','charset':'utf8'}
-    # with pymysql.connect(**config) as conn:
+def init_table(today):
+    config = {'host': "localhost", 'port': 3306, 'password': 'root', 'db': 'rodger', "user": 'root', 'charset': 'utf8'}
+    with pymysql.connect(**config) as cur:
+        for event in event_type_dic.values():
+            table_name = event + '_' + today.replace('-', '_')
+            sql = '''create table if not exists {0} (
+                id int primary key AUTO_INCREMENT ,
+                mand varchar(20),
+                aid varchar(20),
+                detail varchar (20),
+                score varchar (100),
+                type varchar (20),
+                sid varchar (20),
+                uid varchar (20) not null ,
+                time timestamp not null,
+                event varchar(50) 
+                )'''.format(table_name)
+            cur.execute(sql)
 
-    # # print file creation time
-    # print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.stat(FileName).st_atime)))
-    #
-    # # print file modified time
-    # print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.stat(FileName).st_mtime)))
-    # if int(os.stat(FileName).st_atime) == int(os.stat(FileName).st_mtime):
-    #     print('File has not been modified.')
-    # print(os.path.getmtime(FileName))
+
+def data_mining(dir, mand):
+    config = {'host': "localhost", 'port': 3306, 'password': 'root', 'db': 'rodger', "user": 'root', 'charset': 'utf8'}
+    today = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    insert_data = {}
+    for type in event_type_dic.values():
+        insert_data[type] = []
+    for i in get_files_name(dir):
+        record = get_record(os.path.join(dir, i), mand)
+        for data_time, user, event in record:
+            if not isinstance(user, dict):
+                user = json.loads(user)
+            event_type = event[0].split(',')[0]
+            event_name = event_type_dic[event_type]
+            insert_data[event_name].append((mand, user['aid'],
+                                            json.dumps(user['detail']).replace(' ', '').replace('[', '').replace(']',
+                                                                                                                 ''),
+                                            json.dumps(user['score']).replace(' ', '').replace('[', '').replace(']',
+                                                                                                                ''),
+                                            user['type'], user['sid'], user['uid'], data_time, event[0]))
+    for type in event_type_dic.values():
+        table_name = type + '_' + today.replace('-', '_')
+        insert_temp = '''
+            insert into {0} (mand,aid,detail,score,type,sid,uid,time,event)
+            values (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        '''.format(table_name)
+        with pymysql.connect(**config) as cur:
+            cur.executemany(insert_temp, insert_data[type])
+
+
+if __name__ == "__main__":
+    today = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    init_table(today)
+    config = {'host': "localhost", 'port': 3306, 'password': 'root', 'db': 'rodger', "user": 'root', 'charset': 'utf8'}
+
+    mand = 'add_score'
+    # mand = 'batch_add_score'
+    dir = r"E:\sCrpDownload"
+    data_mining(dir,mand)
